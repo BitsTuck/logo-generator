351b3cfb63aedac46dc52172660a6189c839917b
Elizabeth Tucker <elizabethtucker@elizabeths-air.lan>
2023-10-19 16:51:23 -0400

    Initial coding and file structure

---
+++ index.js
@@ -0,0 +1,44 @@
+//Imports inquirer, fs, and the generateMarkdown.js funcionalities into the index.js file.
+const inquirer = require('inquirer');
+const fs = require('fs');
+const {Circle, Square, Triangle} = require('./utils/generateLogo');
+
+newCircle = new Circle ('blt', 'blue', 'red')
+console.log(newCircle)
+console.log(newCircle.newShape())
+// Questions posed by the app, including type and potential answer choices (see: license)
+const questions = [ {
+    type: 'input',
+    message: 'Please enter your initials (up to three):',
+    name: 'initials',
+  },
+  {
+    type: 'input',
+    message: 'Please enter a text color:',
+    name: 'textColor',
+  },
+  {
+    type: 'list',
+    message: 'Please choose a logo shape:',
+    name: 'shape',
+    choices: ['Circle', 'Triangle', 'Square']
+  },
+  {
+      type: 'input',
+      message: 'Please enter a shape color:',
+      name: 'shapeColor',
+    }];
+
+    function generateLogo(data) {
+        return `${logoShape}`
+    }
+    
+    //invokes inquirer and directs the writing of the ReadMe file when the questions have been answered. Error handling included.
+
+inquirer
+  .prompt(questions)
+  .then((response) => {
+    // let newShape = new Circle(response.initials, response.textColor, response.shapeColor); SWITCH CASE GOES HERE
+    fs.writeFile('logo.svg', newShape.newShape(), (err) =>
+    err ? console.log('Please complete the form') : console.log('Thanks! Click on logo.svg to see your logo.'))
+  });
