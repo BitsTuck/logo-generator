dd0b2c825edc51589b98ec6bd4b031b99f933982
Elizabeth Tucker <elizabethtucker@elizabeths-air.lan>
2023-10-19 18:21:27 -0400

    color and shape work

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const {Circle, Square, Triangle} = require('./utils/generateLogo');
 newCircle = new Circle ('blt', 'blue', 'red')
 console.log(newCircle)
 console.log(newCircle.newShape())
+
+newSquare = new Square ('blt', 'blue', 'red')
 // Questions posed by the app, including type and potential answer choices (see: license)
 const questions = [ {
     type: 'input',
@@ -38,7 +40,9 @@ const questions = [ {
 inquirer
   .prompt(questions)
   .then((response) => {
-    // let newShape = new Circle(response.initials, response.textColor, response.shapeColor); SWITCH CASE GOES HERE
+    let newShape = new Circle(response.initials, response.textColor, response.shapeColor); 
+    // let newShape = new Square(response.initials, response.textColor, response.shapeColor)
     fs.writeFile('logo.svg', newShape.newShape(), (err) =>
     err ? console.log('Please complete the form') : console.log('Thanks! Click on logo.svg to see your logo.'))
-  });
+  }); 
+//   SWITCH CASE GOES at letNewShape
--- utils/generateLogo.js
+++ utils/generateLogo.js
@@ -10,29 +10,37 @@ class Shape {
 
 }
 
-class Circle extends Shape { 
-    constructor (text, textColor, color) {
-    super(text, textColor, color)
+class Circle extends Shape {
+    constructor(text, textColor, color) {
+        super(text, textColor, color)
     }
 
-    newShape(){
+    newShape() {
         return (`<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"> <circle cx="150" cy="100" r="80" fill=${this.color} /> <text x="150" y="125" font-size="60" text-anchor="middle" fill=${this.textColor}>${this.text}</text></svg>`)
     }
 }
 
-class Square extends Shape { 
-    constructor (text, textColor, color) {
-    super(text, textColor, color)
+class Square extends Shape {
+    constructor(text, textColor, color) {
+        super(text, textColor, color)
+    }
+
+    newShape() {
+        return (`<svg version "1.1" width="300" height="300" xmlns="http://www.w3.org/2000/svg> <rect width="300" height="300" fill="${this.color}" /> <text x="150" y="125" font-size="60" text-anchor="middle" fill="${this.textColor}">${this.text}</text></svg>`)
     }
 }
 
-class Triangle extends Shape { 
-    constructor (text, textColor, color) {
-    super(text, textColor, color)
+class Triangle extends Shape {
+    constructor(text, textColor, color) {
+        super(text, textColor, color)
+    }
+
+    newShape() {
+        return (`<svg version "1.1" width="300" height="300" xmlns="http://www.w3.org/2000/svg"> <polygon width="300" height="300" polygon points="150, 18 244, 182 56, 182" fill=${this.color} /> <text x="150" y="125" font-size="60" text-anchor="middle" fill=${this.textColor}>${this.text}</text></svg>`)
     }
 }
 
 
 
 
-module.exports = {Circle, Square, Triangle};
+module.exports = { Circle, Square, Triangle };
