7d092ff4705f0c034aa4ba6f82eefdcf50bb65d0
Elizabeth Tucker <elizabethtucker@elizabeths-air.lan>
2023-10-19 20:15:47 -0400

    first pass at switch function

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //Imports inquirer, fs, and the generateMarkdown.js funcionalities into the index.js file.
 const inquirer = require('inquirer');
 const fs = require('fs');
-const {Circle, Square, Triangle} = require('./utils/generateLogo');
+const { Circle, Square, Triangle } = require('./utils/generateLogo');
 
 // newCircle = new Circle ('blt', 'blue', 'red')
 // console.log(newCircle)
@@ -9,40 +9,49 @@ const {Circle, Square, Triangle} = require('./utils/generateLogo');
 
 // newSquare = new Square ('blt', 'blue', 'red')
 // Questions posed by the app, including type and potential answer choices (see: license)
-const questions = [ {
-    type: 'input',
-    message: 'Please enter your initials (up to three):',
-    name: 'initials',
-  },
-  {
-    type: 'input',
-    message: 'Please enter a text color:',
-    name: 'textColor',
-  },
-  {
-    type: 'list',
-    message: 'Please choose a logo shape:',
-    name: 'shape',
-    choices: ['Circle', 'Triangle', 'Square']
-  },
-  {
-      type: 'input',
-      message: 'Please enter a shape color:',
-      name: 'shapeColor',
-    }];
+const questions = [{
+  type: 'input',
+  message: 'Please enter your initials (up to three):',
+  name: 'initials',
+},
+{
+  type: 'input',
+  message: 'Please enter a text color:',
+  name: 'textColor',
+},
+{
+  type: 'list',
+  message: 'Please choose a logo shape:',
+  name: 'shape',
+  choices: ['Circle', 'Square', 'Triangle']
+},
+{
+  type: 'input',
+  message: 'Please enter a shape color:',
+  name: 'shapeColor',
+}];
 
-    function generateLogo(data) {
-        return `${logoShape}`
-    }
-    
-    //invokes inquirer and directs the writing of the ReadMe file when the questions have been answered. Error handling included.
+//invokes inquirer and directs the writing of the ReadMe file when the questions have been answered. Error handling included.
 
 inquirer
   .prompt(questions)
   .then((response) => {
-    let newShape = new Triangle(response.initials, response.textColor, response.shapeColor); 
-    // let newShape = new Square(response.initials, response.textColor, response.shapeColor)
-    fs.writeFile('logo.svg', newShape.newShape(), (err) =>
+  let logoShape =
+      function (response) {
+        switch (response) {
+        case 'Circle':
+          return (new Circle(response.initials, response.textColor, response.shapeColor));
+          break;
+        case 'Square':
+          return (new Square(response.initials, response.textColor, response.shapeColor));
+          break;
+        case 'Triangle':
+          return (new Triangle(response.initials, response.textColor, response.shapeColor));
+          break;
+        default:
+          break;
+      }
+    }
+    fs.writeFile('logo.svg', logoShape.newShape(), (err) =>
     err ? console.log('Please complete the form') : console.log('Thanks! Click on logo.svg to see your logo.'))
-  }); 
-//   SWITCH CASE GOES at letNewShape
+  });
